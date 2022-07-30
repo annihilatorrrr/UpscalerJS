d66c06e2086273b0a93cbdcb9555c11704fbb9e9
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-07-30 08:54:19 -0400

    wip

--- docs2/src/pages/index.tsx
+++ docs2/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import GitHubButton from 'react-github-btn';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,6 +16,7 @@ function HomepageHeader() {
         <h2 className="hero__subtitle">{siteConfig.tagline}</h2>
         <p>Open source, developed by the community, and completely free to use under the MIT license.</p>
         <p>Tags (star, tweet, follow)</p>
+        <GitHubButton href="https://github.com/thekevinscott/upscalerjs" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star thekevinscott/upscalerjs on GitHub">Star</GitHubButton>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -38,23 +40,24 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Upscale images using Javascript with Tensorflow.js">
+      description="Upscale images using Javascript with Tensorflow.js"
+    >
       <HomepageHeader />
-      <main>
+      <section>
         <HomepageFeatures />
-      </main>
-      <main>
+      </section>
+      <section>
         <div style={{ maxWidth: 1200, margin: '0 auto' }}>
-      <iframe src="https://codesandbox.io/embed/github/thekevinscott/upscalerjs/tree/main/examples/basic?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+      <iframe src="https://codesandbox.io/embed/github/thekevinscott/upscalerjs/tree/section/examples/basic?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
         title="basic-use-case-of-upscalerjs"
         sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
       ></iframe>
       <Link href="/docs/usage">See more examples</Link>.
         </div>
-      </main>
-      <main>
+      </section>
+      <section>
       <div>Testimonials! Get addy osmani, jason mayes, david bisset</div>
-      </main>
+      </section>
     </Layout>
   );
 }
